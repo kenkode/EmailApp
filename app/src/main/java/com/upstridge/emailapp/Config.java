cc597adca026649c7ab9af8ed8da65ca5dc8554b
kenkode <wangoken2@gmail.com>
2017-05-11 15:44:17 +0300

    Initial commit

---
+++ app/src/main/java/com/upstridge/emailapp/Config.java
@@ -0,0 +1,10 @@
+package com.upstridge.emailapp;
+
+/**
+ * Created by Wango-PC on 5/11/2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="wangoken2@gmail.com";
+    public static final String PASSWORD ="samsonkennedy";
+        }
